891ca86bc5d7e793fa10a4cfe8674c798ef90cd5
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-03-07 12:20:11 +0900

    *

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,29 +3,9 @@ var yeoman = require('yeoman-generator');
 
 module.exports = yeoman.generators.Base.extend({
   writing: function () {
-    this.fs.copy(
-      this.templatePath('production'),
-      this.destinationPath('production')
-    );
-    this.fs.copy(
-      this.templatePath('site.yml'),
-      this.destinationPath('site.yml')
-    );
-    this.fs.copy(
-      this.templatePath('webservers.yml'),
-      this.destinationPath('webservers.yml')
-    );
-    this.fs.copy(
-      this.templatePath('group_vars/.gitkeep'),
-      this.destinationPath('group_vars/.gitkeep')
-    );
-    this.fs.copy(
-      this.templatePath('host_vars/.gitkeep'),
-      this.destinationPath('host_vars/.gitkeep')
-    );
     this.fs.directory(
-      this.templatePath('roles'),
-      this.destinationPath('roles')
+      this.templatePath('.'),
+      this.destinationPath('.')
     );
   }
 });
